a50df268d0a19b74e36cad1a30c5e699f209b2c0
Tom Ootes <tom@ootes.io>
2025-11-18 15:29:56 +0100

    fix: add YAML support to Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import yaml from '@modyfi/vite-plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), yaml()],
   base: '/publiccode-checker/',
   build: {
     outDir: 'docs',

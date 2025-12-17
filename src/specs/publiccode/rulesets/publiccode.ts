f210b5cf5adb9cfcaf037c448d356eb185395450
Tom Ootes <tom@ootes.io>
2025-12-17 12:26:43 +0100

    feat: use pc.yml JSON schema from developer italia

--- src/specs/publiccode/rulesets/publiccode.ts
+++ src/specs/publiccode/rulesets/publiccode.ts
@@ -1,9 +1,13 @@
 import type { RulesetDefinition } from '@stoplight/spectral-core';
 import remoteSchema from '../../../functions/remoteSchema';
-import publiccodeSchema from '../publiccode_schema.json';
+// import publiccodeSchema from '../publiccode_schema.json';
 
 export const PUBLICCODE_GIT_URI = 'https://github.com/publiccodeyml/publiccode.yml';
 export const PUBLICCODE_DOCS_URL = 'https://yml.publiccode.tools/schema.core.html#key-';
+export const PUBLICCODE_JSON_SCHEMA= 'https://raw.githubusercontent.com/teamdigitale/publiccode-json-schema/refs/heads/main/data/publiccode-schema/publiccode_schema_v0.5.7.json';
+
+const response = await fetch(PUBLICCODE_JSON_SCHEMA);
+const jsonSchemaPublicCode = await response.json();
 
 const publiccode: RulesetDefinition = {
   description: 'Publiccode ruleset for validating publiccode.yml files.',
@@ -17,7 +21,7 @@ const publiccode: RulesetDefinition = {
       then: {
         function: remoteSchema,
         functionOptions: {
-          schema: publiccodeSchema
+          schema: jsonSchemaPublicCode
         },
       },
     },
